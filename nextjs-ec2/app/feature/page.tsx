9e635e52ba4db8ab9690679715ec9dfc60bfa0dc
Ubeyid Oumer <ubeyidah@gmail.com>
2025-08-19 13:01:12 +0300

    create feature page

---
+++ nextjs-ec2/app/feature/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Feature = () => {
+  return (
+    <div>
+      <h1>feature page</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic, dolores
+        quisquam modi ipsam esse, magnam placeat molestias facilis consequuntur
+        quam nobis! At possimus sequi, labore corporis temporibus veritatis rem
+        ipsam.
+      </p>
+    </div>
+  );
+};
+
+export default Feature;
